e0f9de1d117858e40681868b2a7cc5c5d0a25fcd
SpasilliumNexus <SpasilliumNexus@gmail.com>
2018-04-28 11:02:09 -0400

    environment_setup: add lzip to apt install

--- 03-environment_setup.sh
+++ 03-environment_setup.sh
@@ -13,7 +13,7 @@ SUPERRSKITCHEN_REPO=git@gitlab.com:superr/superrs-kitchen3.git
 
 
 # Missing in Ubuntu 18.04: libesd0-dev
-sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev openjdk-8-jdk
+sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzip lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev openjdk-8-jdk
 
 
 mkdir $ANDROID_DIR
